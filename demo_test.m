f45928488479f6d9023b256b793abe43e41675eb
longzhen <310789324@qq.com>
2020-05-06 18:45:27 +0800

    Add files via upload

--- demo_test.m
+++ demo_test.m
@@ -6,7 +6,7 @@ img = imread(filename);
 T= im2double(img);
 size_tensor=size(T);
 Nway =size_tensor;
-ObsRatio=20;                 
+ObsRatio=25;                 
 Omega = randperm(prod(Nway));
 Omega = Omega(1:round((ObsRatio/100)*prod(Nway)));
 O = zeros(Nway);
@@ -18,7 +18,7 @@ known=find(y);
 data=y(known);
 multi1=zeros(Nway);
 multi2=zeros(Nway);
-r = 20;
+r = 50;
 opts.no_noise = 1;
 opts.epsilon = 1e-5;
 opts.beta = 6500;
@@ -36,32 +36,29 @@ for i=1:maxIter
      X=X+Lambda/beta;
      known=find(X);
       data=X(known);
-     AA=tic;
-     [Z,t] =update_Z(size_tensor, r,known, data, opts,X,beta);
-     CP(i)=t;
-      Z=reshape(Z,size_tensor);
+%      AA=tic;
+     [Z,t] =update_Z(size_tensor, r,known, data, opts,X,beta);      
+%      CP(i)=t;
+       Z=reshape(Z,size_tensor);
+       Z(Omega) = T(Omega);
       lambda = 0.99;
-      Z=Z-Lambda/beta;
-      [X,t] = update_X( T,Omega,alpha, beta,lambda,Z );
+      X=Z-Lambda/beta;
+      [X,t] = update_X( T,Omega,alpha, beta,lambda,X );
+       X(Omega) = T(Omega);
       TT(i)=t;
   %    Lambda=zeros(size_tensor);
       Lambda=Lambda+beta*(X-Z);
-      ADMM(i)=toc(AA);
       relative_error(i)=norm(X(:)-Xlast(:),'fro')/norm(T(:),'fro');
-     beta=beta*1.05; 
+%     beta=beta; 
 if(relative_error(i)<1e-5)
     break;
 end
-%  kkk=relative_error(i);
+  kkk=relative_error(i);
  Xlast = X;
 end
 figure;
-subplot(2,2,1),imshow(T),title('orginal image');
-subplot(2,2,2),imshow(y),title('missing image');
-subplot(2,2,3),imshow(X),title('recovered image');
-subplot(2,2,4),
-i=2:1:length(relative_error);
-plot(i,relative_error(i)),title('check convergence'),xlabel('iteration'),ylabel('relative error');
-%   lambda=lambda+5;
+subplot(1,3,1),imshow(T),title('orginal image');
+subplot(1,3,2),imshow(y),title('missing image');
+subplot(1,3,3),imshow(X),title('recovered image');
 
      
